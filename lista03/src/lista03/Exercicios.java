e8e410db8a270458e88e6b7a03c8c0edbec1be3f
1827524 <1827524@DASO113959.educacional.up>
2019-03-12 22:12:49 -0300

    Adicionado a primeira versão da lista de exercícios 03.

---
+++ lista03/src/lista03/Exercicios.java
@@ -0,0 +1,52 @@
+package lista03;
+
+/**
+ * Classe para executar os exercícios da lista 03.
+ * @author DaviMR
+ */
+public class Exercicios {
+
+	/**
+	 * Primeiro fator fixo para utilização no cálculo de peso ideal para homens.
+	 */
+	public static final double FATOR_MASCULINO_1 = 72.7d;
+	
+	/**
+	 * Segundo fator fixo para utilização no cálculo de peso ideal para homens.
+	 */
+	public static final double FATOR_MASCULINO_2 = 58d;
+	
+	/**
+	 * Primeiro fator fixo para utilização no cálculo de peso ideal para mulheres.
+	 */
+	public static final double FATOR_FEMININO_1 = 62.1d;
+	
+	/**
+	 * Segundo fator fixo para utilização no cálculo de peso ideal para mulheres.
+	 */
+	public static final double FATOR_FEMININO_2 = 4.7d;
+	
+	
+	/**
+	 * Ex01 . Calcula o peso ideal do usuário baseado no sexo e na altura.
+	 * @param sexo - M para masculino e F para feminino.
+	 * @param altura - Altura em metros do usuário.
+	 * @return - Peso ideal em Kg.
+	 */
+	public static double pesoIdeal (String sexo, double h) {
+		
+		double pesoIdeal = 0d;
+		
+		if (sexo.equalsIgnoreCase("M")) {
+			
+			pesoIdeal = (FATOR_MASCULINO_1 * h) - FATOR_MASCULINO_2;
+		}
+		
+		if (sexo.equalsIgnoreCase("F")) {
+			
+			pesoIdeal = (FATOR_FEMININO_1 * h) - FATOR_FEMININO_2;
+		}
+		
+		return pesoIdeal;
+	}
+}
---
+++ lista03/src/lista03/Principal.java
@@ -0,0 +1,91 @@
+package lista03;
+
+/**
+ * Essa classe vai servir para a resolução da lista 03 de exercícios.
+ * @author DaviMR
+ * @version 1.0
+ * @since 12/03/2019
+ */
+public class Principal {
+
+	/**
+	 * Função que da início ao programa.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int menu = menu2();
+
+		while (menu > -1) {
+
+			swtch(menu);
+
+			menu = menu2();
+		}
+	}
+
+	/**
+	 * Faz a execução da tela com as opções ao usuário e devolve a escolha.
+	 * @return - Opção selecionada pelo usuário.
+	 */
+	public static int menu2() {
+
+		String[] opcoes = { "" };
+
+		String titulo = "Selecione a opção desejada:";
+
+		String descricao = null;
+
+		int menu = Console.mostrarMenu(opcoes, titulo, descricao);
+
+		return menu;
+
+	}
+
+	/**
+	 * Chama as funções de acordo com a seleção do usuário.
+	 * @param menu - Opção selecionada pelo usuário.
+	 */
+	public static void swtch(int menu) {
+
+		switch (menu) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			
+			break;
+		case 6:
+			
+			break;
+		case 7:
+			
+			break;
+		}
+	}
+	
+	/**
+	 * Faz a execução de tela para o cálculo de peso ideal do usuário.
+	 */
+	public static void pesoIdeal() {
+		
+		//Pega as variaveis do teclado
+		String sexo = Console.recuperaTexto("Digite o sexo M para masculino e F para feminino ");
+		double h = Console.recuperaDecimal("Digite a altura em metros ");
+		
+		//Executa o cálculo
+		double pesoIdeal = Exercicios.pesoIdeal(sexo, h);
+		
+		//Printa na tela
+		System.out.println("O peso ideal é " + pesoIdeal + "\n");
+	}
+}

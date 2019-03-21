cb4cd5bfaf8f8fb848b18916c9f54e45ed523ce9
1827524 <1827524@DASO113959.educacional.up>
2019-03-20 21:55:12 -0300

    Correção de bugs

--- lista02/src/lista02/Exercicios.java
+++ lista02/src/lista02/Exercicios.java
@@ -6,56 +6,63 @@ import java.util.List;
 
 /**
  * Classe para executar os exercícios da lista 02.
+ * 
  * @author DaviMR
  */
 public class Exercicios {
-	
+
 	/**
 	 * Método que cria um vetor de inteiros populado pelo usuário.
+	 * 
 	 * @param tamanhoVet - Tamanho do vetor escolhido pelo usuário.
 	 * @return - Vetor de inteiros populado.
 	 */
-	public static List<Integer>vetorInteiros (int tamanhoVetor) {
-		
+	public static List<Integer> vetorInteiros(int tamanhoVetor) {
+
 		List<Integer> vetorInteiros = new ArrayList<>();
-		
-		for (int i = 0; i < tamanhoVetor; i ++) {
-			
-			vetorInteiros.add(Console.recuperaInteiro("Digite o valor para a posição " + (i+1) + "."));
+
+		for (int i = 0; i < tamanhoVetor; i++) {
+
+			vetorInteiros.add(Console.recuperaInteiro("Digite o valor para a posição " + (i + 1) + "."));
 		}
 		
 		return vetorInteiros;
 	}
-	
+
 	/**
-	 * Ex02 . Método que retorna o valor do maior elemento dentro de um vetor de inteiros definido pelo usuário.
-	 * @param tamanhoVetor - Tamanho do vetor que o usuário predefiniu anteriormente.
+	 * Ex02 . Método que retorna o valor do maior elemento dentro de um vetor de
+	 * inteiros definido pelo usuário.
+	 * 
+	 * @param tamanhoVetor - Tamanho do vetor que o usuário predefiniu
+	 *                     anteriormente.
 	 * @return - Maior elemento do vetor.
 	 */
-	public static int maiorElementoVetor (int tamanhoVetor) {
-		
+	public static int maiorElementoVetor(int tamanhoVetor) {
+
 		List<Integer> vetor = vetorInteiros(tamanhoVetor);
-		
+
 		Integer maior = Collections.max(vetor);
-		
+
 		return maior;
 	}
-	
+
 	/**
-	 * Ex03 . Método que retorna o menor elemento e sua posição em um vetor definido pelo usuário.
+	 * Ex03 . Método que retorna o menor elemento e sua posição em um vetor definido
+	 * pelo usuário.
+	 * 
 	 * @param tamanhoVeto - Tamanho do vetor que o usuário predefiniu anteriormente.
 	 * @return - Menor elemento e sua posição no vetor.
 	 */
 	public static int[] menorElemento(int tamanhoVetor) {
-		
+
 		List<Integer> vetor = vetorInteiros(tamanhoVetor);
-		
-		int[] menorPosicao = new int [2];
-		
-		menorPosicao [0] = Collections.min(vetor);
-		
-		menorPosicao [1] = vetor.indexOf(Collections.min(vetor));
-				
+
+		int[] menorPosicao = new int[2];
+
+		menorPosicao[0] = Collections.min(vetor);
+
+		menorPosicao[1] = vetor.indexOf(Collections.min(vetor));
+
 		return menorPosicao;
 	}
 
--- lista02/src/lista02/Principal.java
+++ lista02/src/lista02/Principal.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 /**
  * Essa classe vai servir para a resolução da lista 02 de exercícios.
+ * 
  * @author DaviMR
  * @version 1.0
  * @since 12/03/2019
@@ -11,11 +12,13 @@ import java.util.List;
 public class Principal {
 
 	/**
-	 * Método que da início ao programa chamando o menu e utilizando a escolha gerada nele.
+	 * Método que da início ao programa chamando o menu e utilizando a escolha
+	 * gerada nele.
+	 * 
 	 * @param args
 	 */
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		int menu = menu2();
 
 		while (menu > -1) {
@@ -25,89 +28,95 @@ public class Principal {
 			menu = menu2();
 		}
 	}
-	
-		/**
-		 * Faz a execução da tela de menu com as opções ao usuário e devolve a escolha.
-		 * @return - Opção selecionada pelo usuário.
-		 */
-		public static int menu2() {
 
-			String[] opcoes = {"Mostra um vetor de inteiros populado pelo usuário.", 
-					"Mostra o maior elemento de um vetor de inteiros populado pelo usuário.", 
-					"Mostra o menor elemento e sua posição em um vetor de inteiro populado pelo usuário."};
-
-			String titulo = "Selecione a opção desejada:";
-
-			String descricao = null;
-
-			int menu = Console.mostrarMenu(opcoes, titulo, descricao);
-
-			return menu;
-		}
-
-		/**
-		 * Chama os métodos de acordo com a seleção do usuário.
-		 * @param menu - Opção selecionada pelo usuário.
-		 */
-		public static void swtch(int menu) {
-
-			switch (menu) {
-			case 1:
-				mostrarVetor();
-				break;
-			case 2:
-				maiorElementoVetor();
-				break;
-			case 3:
-				menorElemento();
-				break;
-			case 4:
-
-				break;
-			case 5:
-
-				break;
-			case 6:
-
-				break;
-			case 7:
-
-				break;
-			}
-		}
-		
 	/**
-	 * Faz a execução de tela para mostrar um vetor de inteiros definido pelo usuário.
+	 * Faz a execução da tela de menu com as opções ao usuário e devolve a escolha.
+	 * 
+	 * @return - Opção selecionada pelo usuário.
+	 */
+	public static int menu2() {
+
+		String[] opcoes = { "Mostra um vetor de inteiros populado pelo usuário.",
+				"Mostra o maior elemento de um vetor de inteiros populado pelo usuário.",
+				"Mostra o menor elemento e sua posição em um vetor de inteiro populado pelo usuário." };
+
+		String titulo = "Selecione a opção desejada:";
+
+		String descricao = null;
+
+		int menu = Console.mostrarMenu(opcoes, titulo, descricao);
+
+		return menu;
+	}
+
+	/**
+	 * Chama os métodos de acordo com a seleção do usuário.
+	 * 
+	 * @param menu - Opção selecionada pelo usuário.
+	 */
+	public static void swtch(int menu) {
+
+		switch (menu) {
+		case 1:
+			mostrarVetor();
+			break;
+		case 2:
+			maiorElementoVetor();
+			break;
+		case 3:
+			menorElemento();
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		case 6:
+
+			break;
+		case 7:
+
+			break;
+		}
+	}
+
+	/**
+	 * Faz a execução de tela para mostrar um vetor de inteiros definido pelo
+	 * usuário.
 	 */
 	public static void mostrarVetor() {
-		
+
 		int tamanhoVet = Console.recuperaInteiro("Quantos elementos irão conter dentro do vetor?");
-		
+
 		List<Integer> vetor = Exercicios.vetorInteiros(tamanhoVet);
-		
+
 		System.out.println(vetor + "\n");
 	}
-	
+
 	/**
-	 * Faz a execução de tela para mostrar o maior elemento de um vetor de inteiros definido pelo usuário.
+	 * Faz a execução de tela para mostrar o maior elemento de um vetor de inteiros
+	 * definido pelo usuário.
 	 */
 	public static void maiorElementoVetor() {
-		
+
 		int tamanhoVetor = Console.recuperaInteiro("Quantos elementos irão conter dentro do vetor?");
-		
+
 		System.out.println(Exercicios.maiorElementoVetor(tamanhoVetor));
 	}
-	
+
 	/**
-	 * Faz a execução de tela para mostrar o menor elemento e sua posição em um vetor definido pelo usuário.
+	 * Faz a execução de tela para mostrar o menor elemento e sua posição em um
+	 * vetor definido pelo usuário.
 	 */
 	public static void menorElemento() {
-		
+
 		int tamanhoVetor = Console.recuperaInteiro("Quantos elementos irão conter dentro do vetor?");
-		
+
 		int posicaoElemento[] = Exercicios.menorElemento(tamanhoVetor);
-		
-		System.out.println("O menor elemento é o " + posicaoElemento[0] + " e sua posição no vetor é " + (posicaoElemento[1]+1) + ".\n\n");
+
+		System.out.println("O menor elemento é o " + posicaoElemento[0] + " e sua posição no vetor é "
+				+ (posicaoElemento[1] + 1) + ".\n\n");
 	}
-	
+
 }

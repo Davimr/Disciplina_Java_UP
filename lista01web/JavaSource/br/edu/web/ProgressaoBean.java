178cbae4577aed15ab61e305f69b860ea8a2796c
1827524 <1827524@DASO113959.educacional.up>
2019-05-16 21:57:07 -0300

    Adicionado codigos

--- lista01web/JavaSource/br/edu/web/ProgressaoBean.java
+++ lista01web/JavaSource/br/edu/web/ProgressaoBean.java
@@ -1,12 +1,12 @@
 package br.edu.web;
 
+import java.util.ArrayList;
+
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.validation.constraints.NotNull;
-
-import br.edu.lista01.Exercicios;
+import entidade.CalculoPA;
 
 /**
  * Calculo da progressao aritmetica
@@ -18,58 +18,42 @@ import br.edu.lista01.Exercicios;
 @RequestScoped
 public class ProgressaoBean {
 	
-	@NotNull
-	private Double a1;
+	private CalculoPA calculoPA;
 	
-	@NotNull
-	private Integer n;
+	private ArrayList<CalculoPA> calculos;
 	
-	@NotNull
-	private Double r;
-	
-	private Double an;
-	
-	public void calcular() {
-		
-		this.an = Exercicios.calcularNEsimoTermo(a1, n, r);
+	public ProgressaoBean() {
+		this.calculoPA = new CalculoPA();
+		this.calculos = new ArrayList<CalculoPA>();
 	}
 	
 	public void calcularComMensagem() {
 		
-		this.calcular();
+		calculoPA.calcularNEsimoTermo();
+		this.calculos.add(this.calculoPA);
 		
 		FacesMessage message = new FacesMessage();
-		message.setSummary("O resultado é: " + this.an);
+		message.setSummary("O resultado é: " + this.calculoPA.getAn());
 		message.setSeverity(FacesMessage.SEVERITY_INFO);
 		FacesContext.getCurrentInstance().addMessage(null, message);
+		
+		this.calculoPA = new CalculoPA();
 	}
 
-	public Double getA1() {
-		return a1;
+	public CalculoPA getCalculoPA() {
+		return calculoPA;
 	}
 
-	public void setA1(Double a1) {
-		this.a1 = a1;
+	public void setCalculoPA(CalculoPA calculoPA) {
+		this.calculoPA = calculoPA;
 	}
 
-	public Integer getN() {
-		return n;
+	public ArrayList<CalculoPA> getCalculos() {
+		return calculos;
 	}
 
-	public void setN(Integer n) {
-		this.n = n;
-	}
-
-	public Double getR() {
-		return r;
-	}
-
-	public void setR(Double r) {
-		this.r = r;
-	}
-
-	public Double getAn() {
-		return an;
+	public void setCalculos(ArrayList<CalculoPA> calculos) {
+		this.calculos = calculos;
 	}
 	
 }
